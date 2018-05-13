0587c49be10fb42d8a76f72c40f1e9d5edb205fa
tinyjin <baram991103@gmail.com>
2018-05-14 01:44:08 +0900

    과제5 했당~

---
+++ 과제5/board/login.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+
+$id = $_POST['id'];
+$pw = $_POST['pw'];
+
+#MySQL 커넥션
+$host = "localhost";
+$user = trim(`echo \$DB_USER`);
+$password = trim(`echo \$DB_PW`);
+$db = trim(`echo \$DB_NAME`);
+
+$conn = mysqli_connect($host, $user, $password, $db);
+
+if(!$conn) {
+  die("Connection failed".mysqli_connect_error());
+}
+
+# 로그인 여부 확인
+function loginCheck() {
+
+  $session_id = $_SESSION['id'];
+
+  if (isset($session_id)) {
+    echo "이미 로그인 되어있습니다. id: $session_id <br>";
+    echo "<input type=\"button\" value=\"로그아웃\" onclick=\"location.href='logout.php'\"/>";
+
+    return True;
+  }
+
+  return False;
+}
+
+# 로그인 성공 함수
+function successLogin($id, $name) {
+
+  echo "<h2>Success Login</h2><br>";
+  echo $name."님 환영합니다.";
+  $_SESSION['id'] = $id;
+}
+
+# 계정 존재 여부 확인
+function existAccount($id, $pw) {
+  global $conn;
+
+  $select_query = "SELECT id, pw, name FROM user where id='$id' and pw='$pw'";
+
+  $result = mysqli_query($conn, $select_query);
+
+  $row = mysqli_fetch_array($result);
+
+  if(isset($row['id'])) {
+
+    successLogin($row['id'], $row['name']);
+
+    return True;
+  }
+
+  return False;
+}
+
+#######메인 코드######
+
+if(loginCheck()) {
+
+}
+else if (!empty($id) && !empty($pw)) {
+
+  if(!existAccount($id, $pw)) {
+    echo "<script>alert('계정 정보가 존재하지 않습니다.'); history.back();</script>";
+  }
+
+}
+else {
+  echo "<script>alert('입력 정보가 올바르지 않습니다.'); history.back();</script>";
+}
+
+mysqli_close($conn);
+
+?>
---
+++ 과제5/board/register.php
@@ -0,0 +1,60 @@
+<?php
+#MySQL 커넥션
+$host = "localhost";
+$user = trim(`echo \$DB_USER`);
+$password = trim(`echo \$DB_PW`);
+$db = trim(`echo \$DB_NAME`);
+
+$conn = mysqli_connect($host, $user, $password, $db);
+
+if(!$conn) {
+  die("Connection failed".mysqli_connect_error());
+}
+
+#이미 가입되어있는지 확인
+function isRegisterUser($user_id) {
+
+  global $conn;
+
+  $select_query = "SELECT id from user where id='$user_id'";
+  $select_result = mysqli_query($conn, $select_query);
+  $select_id = mysqli_fetch_array($select_result)['id'];
+
+  if (isset($select_id)) {
+    return True;
+  }
+
+  return False;
+}
+
+#유저 회원가입
+function registerUser($user_id, $user_pw, $user_name) {
+
+  global $conn;
+
+  $insert_query = "INSERT into user(id,pw,name) values('$user_id', '$user_pw', '$user_name')";
+
+  if(mysqli_query($conn, $insert_query))
+    echo "$user_name : 회원가입이 완료되었습니다.";
+  else
+    echo "ERROR!";
+
+}
+
+$id = $_POST['id']; //아이디
+$pw = $_POST['pw']; //비밀번호
+$name = $_POST['name']; //닉네임
+
+
+######메인코드######
+
+if( !isRegisterUser($id) && !empty($id) && !empty($pw) ) {
+  registerUser($id, $pw, $name);
+}
+else {
+  echo "<script>alert('Error: 아이디 또는 비밀번호를 확인해주세요!'); history.back();</script>";
+}
+
+mysqli_close($conn);
+
+ ?>
